ecc2c223961598cd1a678c8b0390eb64afd66429
jointoclass <lessypark06@gmail.com>
2025-03-31 17:03:03 +0900

    3456

--- src/features/NoticeMain/components/FAQNAVbar.jsx
+++ src/features/NoticeMain/components/FAQNAVbar.jsx
@@ -1,24 +1,5 @@
 import React from "react";
-import Header from "../../Header";
-import WhatsOn from "../../home/components/WhatsOn";
-import HighlightExhibition from "../../home/components/HighlightExhibition";
-import ThemeGallery from "../../home/components/ThemeGallery";
-import ServiceMenu from "../../home/components/ServiceMenu";
-import Footer from "../../Footer";
-import TopButton from "../../home/components/TopButton";
 
-const FAQNAVbar = () => {
-  return (
-    <>
-      <Header />
-      <WhatsOn />
-      <HighlightExhibition />
-      <ThemeGallery />
-      <ServiceMenu />
-      <Footer />
-      <TopButton />
-    </>
-  );
-};
-
-export default FAQNAVbar;
+export default function FAQNAVbar() {
+  return <div>FAQNAVbar</div>;
+}
