407c88ee17c08a3845383de55725b0df4fa662e7
gilsu44145942 <asdf441467@gmail.com>
2025-03-31 18:17:35 +0900

    03/31

---
+++ src/features/TopMenu/TopMenu.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../Header";
+import Footer from "../Footer";
+
+function TopMenu() {
+  return (
+    <div>
+      <Header />
+      <h1>탑메뉴전체</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default TopMenu;
