61b8b64d970802d2fc65383ddb6be87a76f81542
jointoclass <lessypark06@gmail.com>
2025-03-31 14:11:43 +0900

    678

---
+++ src/features/notice/components/FAQNAVbar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function FAQNAVbar() {
+  return <div>FAQNAVbar</div>;
+}
---
+++ src/features/notice/components/NoticeModify.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NoticeModify() {
+  return <div>NoticeModify</div>;
+}
