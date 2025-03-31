92c9ec1fe3da24e5df15919aa5c2f2a166be8de6
gilsu44145942 <asdf441467@gmail.com>
2025-03-31 19:08:09 +0900

    03/31

--- src/features/ArtistGallery/ArtistGallery.jsx
+++ src/features/ArtistGallery/ArtistGallery.jsx
@@ -3,11 +3,31 @@ import Header from "../Header";
 import Datacontrol from "./components/Datacontrol";
 import ArtistGalleryLsit from "./components/ArtistGalleryLsit";
 import Footer from "../Footer";
+import styled from "styled-components";
+
+const TitleContainer = styled.div`
+  width: 100%;
+  position: relative;
+`;
+const BackTitle = styled.h1`
+  font-size: 140px;
+  text-align: center;
+  color: #deeaff;
+  padding-top: 60px;
+`;
+const Title = styled.h1`
+  font-size: 90px;
+  text-align: center;
+`;
 
 function ArtistGallery() {
   return (
     <div>
       <Header />
+      <TitleContainer>
+        <BackTitle>ARTIST GALLERY</BackTitle>
+        <Title>ARTIST GALLERY</Title>
+      </TitleContainer>
       <Datacontrol />
       <ArtistGalleryLsit />
       <Footer />
