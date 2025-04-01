bbad77ff3c1c374836d91d80bbc937af11479edd
RYU185 <fence365@naver.com>
2025-04-01 12:09:36 +0900

    123

--- src/features/Notice/Notice.jsx
+++ src/features/Notice/Notice.jsx
@@ -1,29 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Header from "../Header";
 import Footer from "../Footer";
 
 function Notice() {
-  const navigate = useNavigate();
   return (
     <div>
       <Header />
-
-      <Container>
-        <NavList>
-          <Button onClick={() => navigate("/notice")}>공지사항</Button>
-          <Button onClick={() => navigate("/guide")}>시설안내</Button>
-          <Button onClick={() => navigate("/contactus")}>오시는 길</Button>
-          <Button onClick={() => navigate("/faq")}>고객센터</Button>
-        </NavList>
-
-        <Search>
-          <Button></Button>
-          <Button></Button>
-        </Search>
-
-        <NoticeList />
-      </Container>
-
+      <h1>Notice</h1>
       <Footer />
     </div>
   );
