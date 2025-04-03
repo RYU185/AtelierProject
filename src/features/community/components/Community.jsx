9b97d63160186e61db69c3496320acc0d5b0f845
gilsu44145942 <asdf441467@gmail.com>
2025-04-03 21:24:27 +0900

    04/03

--- src/features/community/components/Community.jsx
+++ src/features/community/components/Community.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { BsThreeDots } from "react-icons/bs"; // 점 3개 아이콘
+import { BsThreeDots, BsHeart, BsHeartFill, BsChat } from "react-icons/bs";
 
 const Container = styled.div`
   width: 600px;
@@ -8,8 +8,9 @@ const Container = styled.div`
   border-radius: 8px;
   background: white;
   box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
-  margin: 20px auto;
+  margin: 0 auto;
   position: relative;
+  margin-bottom: 50px;
 `;
 
 const Header = styled.div`
@@ -25,14 +26,14 @@ const UserInfo = styled.div`
 `;
 
 const Nickname = styled.span`
-  font-size: 14px;
+  font-size: 20px;
   font-weight: bold;
   color: #777;
 `;
 
 const DateText = styled.span`
-  font-size: 12px;
-  color: #aaa;
+  font-size: 13px;
+  color: #a0a0a0;
 `;
 
 const Divider = styled.hr`
@@ -54,22 +55,58 @@ const MenuIcon = styled(BsThreeDots)`
   cursor: pointer;
 `;
 
-function Community() {
+const Actions = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  margin-top: 15px;
+`;
+
+const ActionIcon = styled.div`
+  font-size: 24px;
+  margin-right: 25px;
+  color: #ff6347;
+  cursor: pointer;
+  transition: transform 0.1s ease-in-out;
+
+  &:active {
+    transform: scale(1.3); // 클릭 시 크기 확대
+  }
+`;
+
+const ChatIcon = styled(BsChat)`
+  font-size: 24px;
+  color: #888;
+  cursor: pointer;
+  transition: transform 0.3s ease;
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
+
+function Community({ id, nickname, datetext, content }) {
+  const [isHeartFilled, setIsHeartFilled] = useState(false);
+
+  const toggleHeart = () => {
+    setIsHeartFilled(!isHeartFilled);
+  };
+
   return (
     <Container>
       <Header>
         <UserInfo>
-          <Nickname>귀염둥이</Nickname>
-          <DateText>2025.03.27 12:00</DateText>
+          <Nickname>{nickname}</Nickname>
+          <DateText>{datetext}</DateText>
         </UserInfo>
         <MenuIcon />
       </Header>
       <Divider />
-      <Content>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s
-      </Content>
+      <Content>{content}</Content>
+      <Actions>
+        <ActionIcon onClick={toggleHeart}>
+          {isHeartFilled ? <BsHeartFill /> : <BsHeart />}
+        </ActionIcon>
+        <ChatIcon />
+      </Actions>
     </Container>
   );
 }
--- src/features/community/components/CommunityList.jsx
+++ src/features/community/components/CommunityList.jsx
@@ -3,21 +3,112 @@ import Community from "./Community";
 import styled from "styled-components";
 
 const Container = styled.div`
-  width: 50%;
-  height: 800px;
+  width: 40%;
+  height: auto;
   background-color: #e5f0fb;
+  border-radius: 15px;
   margin: 0 auto;
+  margin-bottom: 80px;
 `;
-const Button = styled.button``;
+const ButtonBox = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+const Button = styled.div`
+  color: #018ec8;
+  background-color: #ffffff;
+  font: bold;
+  margin: 60px 20px;
+  padding: 25px;
+  border-radius: 15px;
+  transition: transform 0.3s ease;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.05);
+    border: 1px solid #0b5671;
+  }
+`;
+const DrawwButton = styled.div`
+  color: #ffffff;
+  background-color: #018ec8;
+  font: bold;
+  margin: 60px 20px;
+  padding: 25px;
+  border-radius: 15px;
+  transition: transform 0.3s ease;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.05);
+    border: 1px solid #0c0c0c;
+  }
+`;
+
+const Gird = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  max-height: 600px;
+`;
 
 function CommunityList() {
+  const communityItems = [
+    {
+      id: 1,
+      nickname: "귀염둥이",
+      datetext: "2025.03.27 12:00",
+      content:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s ",
+    },
+    {
+      id: 2,
+      nickname: "둘리",
+      datetext: "2025.03.28 13:00",
+      content:
+        "두번째 게시글 입니다. 두번째 게시글 입니다. 두번째 게시글 입니다. 두번째 게시글 입니다. 두번째 게시글 입니다. ",
+    },
+    {
+      id: 3,
+      nickname: "마이콜",
+      datetext: "2025.03.29 14:00",
+      content:
+        "세번째 게시글 입니다. 세번째 게시글 입니다. 세번째 게시글 입니다. 세번째 게시글 입니다. 세번째 게시글 입니다. ",
+    },
+    {
+      id: 4,
+      nickname: "귀염둥이",
+      datetext: "2025.03.27 12:00",
+      content:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s ",
+    },
+    {
+      id: 5,
+      nickname: "곽철용",
+      datetext: "2025.03.28 13:00",
+      content:
+        "안녕하십니까 묻고 더블로가 마포대교는 무너졌냐 내 순정을 무시하면 그땐 나도 깡패가 되는거야야  ",
+    },
+    {
+      id: 6,
+      nickname: "예림림",
+      datetext: "2025.03.29 14:00",
+      content: "쏠수 있어 진짜 쏠수 있어 ",
+    },
+  ];
   return (
     <div>
       <Container>
-        <Button>게시글 등록</Button>
-        <Button>나의 글 보기</Button>
-        <Button>작품 그리기</Button>
-        <Community />
+        <ButtonBox>
+          <Button>게시글 등록</Button>
+          <Button>나의 글 보기</Button>
+          <DrawwButton>작품 그리기</DrawwButton>
+        </ButtonBox>
+        <Gird>
+          {communityItems.map((item) => (
+            <div key={item.id}>
+              <Community {...item} />
+            </div>
+          ))}
+        </Gird>
       </Container>
     </div>
   );
