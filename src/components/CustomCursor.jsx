b11e4a9f94f14153d7841c0e6d9b7fd7626c668f
RYU185 <fence365@naver.com>
2025-04-06 15:39:48 +0900

    123

--- src/components/CustomCursor.jsx
+++ src/components/CustomCursor.jsx
@@ -29,8 +29,8 @@ const CustomCursor = () => {
     };
 
     const animateCursor = () => {
-      cursorRef.current.x += (targetRef.current.x - cursorRef.current.x) * 0.08;
-      cursorRef.current.y += (targetRef.current.y - cursorRef.current.y) * 0.08;
+      cursorRef.current.x += (targetRef.current.x - cursorRef.current.x) * 0.070;
+      cursorRef.current.y += (targetRef.current.y - cursorRef.current.y) * 0.070;
 
       // 커서 위치 업데이트
       const cursorDot = document.querySelector(".cursor-dot");
