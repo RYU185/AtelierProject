71a7ebf817d2fcfcea49c77a58ee76135291deec
joung886 <rkdcjf311@naver.com>
2025-04-28 17:14:38 +0900

    all

--- src/features/Notification/ReservationNotificationComponent.jsx
+++ src/features/Notification/ReservationNotificationComponent.jsx
@@ -4,35 +4,11 @@ import useWebSocket from "../../socket/useWebSocket";
 import { useEffect } from "react";
 
 const ReservationNotificationComponent = () => {
-  const { addNotification, reservationAlarms } = useNotification();
+  const { reservationAlarms } = useNotification();
   const { token } = useAuth();
   const { client } = useWebSocket();
 
-  useEffect(() => {
-    if (!client || !token || !client.connected) {
-      console.warn("🚫 아직 소켓 연결 안됨 - 구독 시도 안 함");
-      return;
-    }
-
-    const subscription = client.subscribe(
-      "/user/queue/notifications",
-      (message) => {
-        try {
-          const notification = JSON.parse(message.body);
-          console.log("[Notification]", notification);
-          addNotification(notification);
-        } catch (error) {
-          console.error("[Notification Parsing Error]", error);
-        }
-      }
-    );
-
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, [client, token, client?.connected]);
-
-  return null;
+  return null; // 아무 UI나 구독(subscribe) 필요 없음
 };
 
 export default ReservationNotificationComponent;
